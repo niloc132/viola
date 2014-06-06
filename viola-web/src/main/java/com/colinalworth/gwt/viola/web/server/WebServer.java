656f762fd31d7bd9265156831782df7ed413f9b1
Colin Alworth <niloc132@gmail.com>
2014-06-05 19:19:18 -0500

    Stop using never-ending threadpool, restrict to a number of threads less than what will murder couch

--- viola-web/src/main/java/com/colinalworth/gwt/viola/web/server/WebServer.java
+++ viola-web/src/main/java/com/colinalworth/gwt/viola/web/server/WebServer.java
@@ -3,16 +3,14 @@ package com.colinalworth.gwt.viola.web.server;
 import com.colinalworth.gwt.viola.ioc.ViolaModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import rxf.server.BlobAntiPatternObject;
 import rxf.server.RelaxFactoryServer;
 
 import java.io.IOException;
-import java.util.concurrent.Executors;
 
 public class WebServer {
 
 	public static void main(String[] args) throws Exception {
-		BlobAntiPatternObject.EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+//		BlobAntiPatternObject.EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 //		BlobAntiPatternObject.DEBUG_SENDJSON = true;
 
 		Injector i = Guice.createInjector(new ViolaModule(), new ViolaWebModule());
